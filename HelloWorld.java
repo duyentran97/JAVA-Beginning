d6782c1c07db8000b893ffc5fbb652de22ad3ea3
duyentran97 <31406311+duyentran97@users.noreply.github.com>
2017-08-30 21:02:54 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package review;
+
+/**
+ *
+ * @author Hp 8470p
+ */
+
+
+    
+    public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello World");
+    }
+
+}
+
+    
+    
+
